6c3423874c36b77cac0eda1fdc7657a075413480
Aljaz <aljaz.blazej@gmail.com>
2017-12-14 08:21:52 +0100

    initial commit

---
+++ payments/src/main/java/HealthCheckBean.java
@@ -0,0 +1,40 @@
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Logger;
+
+/**
+ * Created by Aljaz on 22/11/2017.
+ */
+
+@Health
+@ApplicationScoped
+@ConfigBundle("profile-config")
+public class HealthCheckBean implements HealthCheck {
+
+    private static final String url = "https://paypal.com";
+
+    private static final Logger LOG = Logger.getLogger(HealthCheckBean.class.getSimpleName());
+
+    @Override
+    public HealthCheckResponse call() {
+        try {
+
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+
+            if (connection.getResponseCode() == 200) {
+                return HealthCheckResponse.named(HealthCheckBean.class.getSimpleName()).up().build();
+            }
+        } catch (Exception exception) {
+            LOG.severe(exception.getMessage());
+        }
+        return HealthCheckResponse.named(HealthCheckBean.class.getSimpleName()).down().build();
+    }
+}
---
+++ payments/src/main/java/PaymentsResource.java
@@ -0,0 +1,59 @@
+import com.codahale.metrics.Counter;
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
+import com.kumuluz.ee.logs.cdi.Log;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.glassfish.jersey.process.internal.RequestScoped;
+import org.eclipse.microprofile.health.Health;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Aljaz on 25/10/2017.
+ */
+@Log
+@RequestScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("profile")
+@Health
+public class PaymentsResource {
+
+    static boolean running = true;
+
+    public static boolean isRunning() {
+        return running;
+    }
+
+    @POST
+    @Metered(name = "packets-delivered-meter")
+    @Path("packets-delivered")
+    public Response packetsDelivered() {
+
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("kill-service")
+    public Response killService() {
+        running = !running;
+        return Response.ok(running).build();
+    }
+
+    @GET
+    @Path("fibonacci-load/{num}")
+    public Response fibonacciLoad(@PathParam("num") int num) {
+        return Response.ok(fibonacci(num)).build();
+    }
+
+    private int fibonacci(int n)  {
+        if (n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
